3530cfe822846d4383182619f930d300734e3e0d
Aman Rawat <ar5941415@gmail.com>
2026-06-01 23:47:19 +0530

    adding update Assistant controller for adding Avatar and Name

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -14,4 +14,29 @@ export const getCurrentUser = async (req, res) => {
     }
 }
 
- 
+ export const updateAssistant = async (req,res)=>{
+    try{
+        const userId = req.userId;
+        const{ AssistantName ,ImgUrl}= req.body;
+       
+        let assistantImage;
+
+        if(req.file){
+            assistantImage = await uploadOnCloudinary(req.file.path);
+        }else{
+            assistantImage = ImgUrl;
+        }
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+        user.assistantName = AssistantName || user.assistantName;
+        user.assistantImage = assistantImage || user.assistantImage;
+        await user.save();
+        res.status(200).json({message: "Assistant data updated successfully"});
+
+    }catch(error){
+        res.status(500).json({ message: `Error updating assistant data: ${error.message}` });
+    }
+ }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getCurrentUser } from '../controllers/user.controllers.js';
+import { getCurrentUser,updateAssistant } from '../controllers/user.controllers.js';
 import { isAuth } from '../middlewares/isAuth.js';
 const userRouter = express.Router();
 
 userRouter.route("/current").get( isAuth,getCurrentUser);
+userRouter.route("/assistant").put(isAuth,updateAssistant);
 
-export default userRouter;
+export default userRouter; 
